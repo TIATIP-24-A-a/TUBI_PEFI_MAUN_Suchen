c3391dc4c9093568a9f6a1d1a861be459054753f
Pedro <pedro.figueiredo@edu.teko.ch>
2024-11-16 16:43:02 +0100

    Refactoring Search Text

--- search.py
+++ search.py
@@ -1,10 +1,11 @@
 def searchText(text, searchText):
-
     occurrences = 0
     currentIndex = 0
     while currentIndex < len(text):
-        if text[currentIndex] == searchText:
-            occurrences += 1
+        if text[currentIndex] == searchText[0]:
+            if text[currentIndex:currentIndex+len(searchText)] == searchText:
+                occurrences += 1
+                currentIndex += len(searchText)-1
 
         currentIndex += 1
 
--- search_tests.py
+++ search_tests.py
@@ -5,15 +5,15 @@ class SearchTextTestCase(unittest.TestCase):
 
     def test_searchText_A_in_ABC_expectOneOccurrence(self):
         result = search.searchText("ABC", "A")
-        self.assertEqual(result, 1)
+        self.assertEqual(1, result)
 
     def test_searchText_A_in_AABC_expectTwoOccurrences(self):
         result = search.searchText("AABC", "A")
-        self.assertEqual(result, 2)
+        self.assertEqual(2, result)
 
-    def test_searchText_BC_in_AABC_expectTwoOccurrences(self):
-        result = search.searchText("AABCBCBC", "BC")
-        self.assertEqual(result, 3)
+    def test_searchText_BC_in_AABCAA_expectOneOccurrence(self):
+        result = search.searchText("AABCAA", "BC")
+        self.assertEqual(1, result)
 
 
 if __name__ == '__main__':
